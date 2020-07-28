4c660b5a7a48aad96ee646769ac88dde7425c7ca
Irenia <15000616693@163.com>
2020-07-28 15:07:10 +0800

    deploy

---
+++ precache-manifest.108243465cbf89eb2d6ff0d1f23f942c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f25163676b64881a48e",
+    "url": "/susuBill_view/css/app.f8ed3305.css"
+  },
+  {
+    "revision": "0e341b6e2c6c727b5f0f",
+    "url": "/susuBill_view/css/chunk-vendors.b49c8f1f.css"
+  },
+  {
+    "revision": "629d8802cf5cd1eb31de84b3f5c613c0",
+    "url": "/susuBill_view/img/qrCode.629d8802.png"
+  },
+  {
+    "revision": "ebbff94305dd8555dfc6b06279d72405",
+    "url": "/susuBill_view/index.html"
+  },
+  {
+    "revision": "8f25163676b64881a48e",
+    "url": "/susuBill_view/js/app.e266b79e.js"
+  },
+  {
+    "revision": "0e341b6e2c6c727b5f0f",
+    "url": "/susuBill_view/js/chunk-vendors.9a6a3bed.js"
+  },
+  {
+    "revision": "b67c1cf05c642a9bb5ab49ab753a5353",
+    "url": "/susuBill_view/js/tagData.json"
+  },
+  {
+    "revision": "e456ff1090ac7d3027e78d9e742151b2",
+    "url": "/susuBill_view/js/tagRecord.json"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/susuBill_view/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/susuBill_view/robots.txt"
+  }
+]);
